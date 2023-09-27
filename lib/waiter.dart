9d65d93b20f9f733075f9287404ee35f180a398d
mbfakourii <mbfakourii@gmail.com>
2023-09-27 14:42:54 +0330

    Fix

--- lib/waiter.dart
+++ lib/waiter.dart
@@ -1,4 +1,4 @@
 export 'src/controllers/waiter_controller.dart';
 export 'src/models/waiter_model.dart';
-export 'src/utils/language.dart';
+export 'src/utils/waiter_language.dart';
 export 'src/widgets/waiter.dart';
